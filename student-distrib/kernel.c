d49f4ccdf857ca90288ba40ea77b849980bff74b
{Grin-ZW} <{2109687622@qq.com}>
2023-03-10 21:46:07 -0600

    0.1

--- student-distrib/kernel.c
+++ student-distrib/kernel.c
@@ -143,6 +143,8 @@ void entry(unsigned long magic, unsigned long addr) {
     /* Init the PIC */
     i8259_init();
 
+    //init rtc
+
     /* Initialize devices, memory, filesystem, enable device interrupts on the
      * PIC, any other initialization stuff... */
 
