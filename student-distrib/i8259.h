e2836e5483285829a5776952b5d0e622b411e8b8
kevinle350 <74153829+kevinle350@users.noreply.github.com>
2023-03-12 14:56:20 -0500

    Pushing PIC

--- student-distrib/i8259.h
+++ student-distrib/i8259.h
@@ -12,15 +12,20 @@
 #define MASTER_8259_PORT    0x20
 #define SLAVE_8259_PORT     0xA0
 
+#define MASTER_COMMAND      MASTER_8259_PORT 
+#define MASTER_DATA         MASTER_8259_PORT + 1
+#define SLAVE_COMMAND       SLAVE_8259_PORT 
+#define SLAVE_DATA          SLAVE_8259_PORT + 1
+
 /* Initialization control words to init each PIC.
  * See the Intel manuals for details on the meaning
  * of each word */
-#define ICW1                0x11
-#define ICW2_MASTER         0x20
-#define ICW2_SLAVE          0x28
-#define ICW3_MASTER         0x04
-#define ICW3_SLAVE          0x02
-#define ICW4                0x01
+#define ICW1                0x11    // start init, edge-triggered inputs, cascade mode, 4 ICWs
+#define ICW2_MASTER         0x20    // high bits of vector, vector offset
+#define ICW2_SLAVE          0x28    // high bits of vector, vector offset
+#define ICW3_MASTER         0x04    // bit vector of seconday PIC, Tell it how it is wired to master/slaves
+#define ICW3_SLAVE          0x02    // input pin on primary PIC, Tell it how it is wired to master/slaves
+#define ICW4                0x01    // ISA=x86, normal/auto EOI, Gives additional information about the environment
 
 /* End-of-interrupt byte.  This gets OR'd with
  * the interrupt number and sent out to the PIC
