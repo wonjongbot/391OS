1e3d3b95ca3ee9709f6161854c8cb5f2ada07156
wonjongbot <13744405+wonjongbot@users.noreply.github.com>
2023-03-13 15:32:40 -0500

    fixed rtc

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -8,34 +8,43 @@
 #include "types.h"
 
 
-void init_rtc(void) {
+void rtc_init(void) {
     unsigned long flags;
 
     /* Turning on periodic interrupt: IRQ8 */
     cli_and_save(flags);    // Disable interrupts
-    outb(REG_B, RTC);       // Select Register B and disable NMI
+    outb(REG_B | 0x80, RTC);       // Select Register B and disable NMI
     char prev = inb(CMOS);  // Read current value of register B
-    outb(REG_B, RTC);       // Set index again (a read will reset the index to register D)
+    outb(REG_B | 0x80, RTC);       // Set index again (a read will reset the index to register D)
     outb(prev | 0x40, CMOS);    // Write previous value ORed with 0x40. This turns on bit 6 of register B
     restore_flags(flags);
-    enable_irq(RTC_IRQ);          // Turn on IRQ8
 
+    enable_irq(RTC_IRQ);          // Turn on IRQ8
+}
+
+void rtc_set_freq(int32_t rate){
+    unsigned long flags;
     /* Changing Interrupt Rate */
-    int32_t rate = 0;   // Not sure what to init the rate to
     rate &= 0x0F;   // rate must be above 2 and not over 15
     cli_and_save(flags);
-    outb(REG_A, RTC);   // Set index to register A, disable NMI
+    outb(REG_A | 0x80, RTC);   // Set index to register A, disable NMI
     char prev_2 = inb(CMOS);  // Get initial value of register A
-    outb(REG_A, RTC);   // Reset index to A
+    outb(REG_A | 0x80, RTC);   // Reset index to A
     outb((prev_2 & 0xF0) | rate, CMOS);   // Write only our rate to A. Rate is bottom 4 bits
     restore_flags(flags);
 }
 
-void handler_rtc(void) {
+void rtc_handler(void) {
 
     /* Need these two lines or interrupt will not happen again to get another interrupt*/
+    unsigned long flags;
+    cli_and_save(flags);    // Disable interrupts
+
     outb(REG_C, RTC);	// select register C
     inb(CMOS);		    // just throw away contents
-    
     send_eoi(RTC_IRQ);
+
+    putc_rtc();
+
+    restore_flags(flags);
 }
--- student-distrib/rtc.h
+++ student-distrib/rtc.h
@@ -6,13 +6,15 @@
 
 #define RTC             0x70    
 #define CMOS            0x71
-#define REG_A           0x8A
-#define REG_B           0x8B
-#define REG_C           0x8C
-#define RTC_IRQ         8
+#define REG_A           0xA
+#define REG_B           0xB
+#define REG_C           0xC
+#define RTC_IRQ         0x8
 
 /* Initializes RTC */
 void rtc_init(void);
+
+void rtc_set_freq(int32_t);
 
 /* Handles interrupts */
 void rtc_handler(void);
