5258247135adddbf6a2b11db4d12e54e9a590d3d
wonjongbot <13744405+wonjongbot@users.noreply.github.com>
2023-04-27 18:37:41 -0500

    schedule up to date

--- student-distrib/schedule.c
+++ student-distrib/schedule.c
@@ -9,20 +9,18 @@ static int pid_to_sched_unit[3] = {-1, -1, -1};
 #endif
 
 void save_terminal(uint32_t terminal_idx){
-    memcpy(VGA_TERM_0 + 0x1000*terminal_idx, VGA_TEXT_BUF_ADDR, 0x1000);
+    //memcpy(VGA_TERM_0 + 0x1000*terminal_idx, VGA_TEXT_BUF_ADDR, 0x1000);
     terminal_arr[terminal_idx].cur_x = get_x();
     terminal_arr[terminal_idx].cur_y = get_y();
-    memcpy(terminal_arr[terminal_idx].text_buf, kb_buf, 128);
     terminal_arr[terminal_idx].rtc_state = get_rtc_counter();
     terminal_arr[terminal_idx].rtc_target = get_rtc_target;
     terminal_arr[terminal_idx].curr_pcb = current;
 }
 
 void recover_terminal(uint32_t terminal_idx){
-    memcpy(VGA_TEXT_BUF_ADDR, VGA_TERM_0 + 0x1000*terminal_idx, 0x1000);
+    //memcpy(VGA_TEXT_BUF_ADDR, VGA_TERM_0 + 0x1000*terminal_idx, 0x1000);
     set_x(terminal_arr[terminal_idx].cur_x);
     set_y(terminal_arr[terminal_idx].cur_y);
-    memcpy(kb_buf, terminal_arr[terminal_idx].text_buf, 128);
     set_rtc_counter(terminal_arr[terminal_idx].rtc_state);
     set_rtc_target(terminal_arr[terminal_idx].rtc_target);
 
@@ -37,15 +35,14 @@ void terminal_switch(uint32_t to){
     terminal* terminal = &terminal_arr[to];
     printf("SWITCHING TO TERMINAL #%d\n",to);
 
-    switch(to){
-        case 0:
-            break;
-        case 1:
-            break;
-        case 2:
-            break;
-        default: break;
-    }
+    // save current terminal content to curr_term_displayed
+    memcpy(VGA_TERM_0 + 0x1000*curr_term_displayed, VGA_TEXT_BUF_ADDR, 0x1000);
+    memcpy(terminal_arr[curr_term_displayed].text_buf, kb_buf, 128);
+
+    // recover terminal cntent from buffer
+    memcpy(VGA_TEXT_BUF_ADDR, VGA_TERM_0 + 0x1000*to, 0x1000);
+    memcpy(kb_buf, terminal_arr[to].text_buf, 128);
+    curr_term_displayed = to;
 }
 
 
@@ -76,7 +73,9 @@ void context_switch(uint32_t to){
     }
     else{
         // if not save esp and ebp of current terminal, then recover the next terminal
+        save_terminal(curr_term_sched);
         curr_term_sched = to;
+        recover_terminal(curr_term_sched);
 
         //update TSS to the stack we are going to
         // set TSS to parent's info
@@ -95,14 +94,18 @@ void context_switch(uint32_t to){
             map_4KB_page(VGA_TEXT_BUF_ADDR, VGA_TERM_0 + 0x1000 * to);
         }
 
+        uint32_t esp_tmp, ebp_tmp;
+
         // save current ebp and esp
         asm volatile(
             "movl %%esp, %0                         \n\t"
             "movl %%ebp, %1                         \n\t"
-            : "+r" (terminal_arr[curr_term_sched].esp_sched), "+r" (terminal_arr[curr_term_sched].ebp_sched)
+            : "+r" (esp_tmp), "+r" (ebp_tmp)
             :
             : "cc"
         );
+        terminal_arr[curr_term_sched].esp_sched = esp_tmp;
+        terminal_arr[curr_term_sched].ebp_sched = ebp_tmp;
         asm volatile(
             "movl %0, %%esp                         \n\t"
             "movl %1, %%ebp                         \n\t"
